be227f0c6fea83480b8b2c3fb192adfd1e406a28
Mazil Khatib <mazilkhatib786@gmail.com>
2024-10-30 11:39:26 +0530

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://passport-ocr-pied.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
